1ba2e016c63b7036e0f17da1587b4edcde719d51
Clement <clementnyankuadjei@gmail.com>
2025-06-26 09:28:34 +0000

    Update Angular project configuration to use @ngx-env/builder for build and serve processes. Refactor environment variables to utilize process.env for Supabase integration. Remove deprecated development environment file and adjust styles and polyfills formatting for consistency.

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://ifnjapuqeeyaxbxwxfpc.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbmphcHVxZWV5YXhieHd4ZnBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2OTQ2ODcsImV4cCI6MjA2NjI3MDY4N30.qu7fXnfIdx0MU-tMRaNmnM7MxnDZyDE-0mMbcp0_I_c',
-};
